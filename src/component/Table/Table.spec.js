4450b29f06fb382c9eecad21c3bb5e78bc960420
Dominik Sumer <domi.sumer@gmail.com>
2017-05-16 08:16:06 +0200

    removed App.test.js for better coverage results Table: added more testcases

--- src/component/Table/Table.spec.js
+++ src/component/Table/Table.spec.js
@@ -1,11 +1,24 @@
 import React from "react";
-import {shallow} from "enzyme";
+import {shallow, mount} from "enzyme";
 import Table from "./Table";
 
 describe('The Table Component', () => {
-    it('should render the empty text.', () => {
+    it('should render the empty text', () => {
         const emptyText ='eeeempty';
         const wrapper = shallow(<Table data={[]} header={[]} emptyText={emptyText}/>);
         expect(wrapper.childAt(0).childAt(1).text()).toEqual(emptyText);
+    });
+
+    it('should pass correct mobileWidth', () => {
+        const mobileWidth = 123;
+        const wrapper = mount(<Table data={[]} header={[]} mobileWidth={mobileWidth}/>);
+        expect(wrapper.find('.table').childAt(0).prop('mobileWidth')).toBe(mobileWidth);
+        expect(wrapper.find('Header').prop('mobileWidth')).toBe(mobileWidth);
+        expect(wrapper.find('Body').prop('mobileWidth')).toBe(mobileWidth);
+    });
+
+    it('should calculate the correct page count', () => {
+        const wrapper = shallow(<Table data={[1,2,3,5,6,7,8]} header={[]} itemsPerPage={3}/>);
+        expect(wrapper.find('Pagination').prop('count')).toBe(3);
     });
 });
